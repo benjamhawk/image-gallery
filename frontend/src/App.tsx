a0fa6f70b3d9ec278cb09e5b86f15830947b2122
Benjamin Hawkins <benjamhawk@gmail.com>
2024-06-18 09:59:12 -0500

    Skaffold file uploader with UploadCare

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { FileUploaderRegular } from "@uploadcare/react-uploader";
+import "@uploadcare/react-uploader/core.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [files, setFiles] = useState<Array<File>>([]);
+
+  console.log(files);
+
+  const handleChangeEvent = (items: any) => {
+    setFiles([
+      ...items.allEntries.filter((file: File) => file.status === "success"),
+    ]);
+  };
 
   return (
-    <>
+    <div>
+      <FileUploaderRegular
+        onChange={handleChangeEvent}
+        pubkey="d8b139264282b18b0844"
+        onFileAdded={() => {
+          console.log("File added");
+        }}
+      />
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {files.map((file) => (
+          <div key={file.uuid}>
+            <img src={file.cdnUrl} alt={file.fileInfo.originalFilename} />
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+type File = {
+  uuid: string;
+  cdnUrl: string;
+  fileInfo: {
+    originalFilename: string;
+  };
+  status: string;
+};
