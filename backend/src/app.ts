ffdd305ef91d88c891920ac4c5ea3912b856cac3
Benjamin Hawkins <benjamhawk@gmail.com>
2024-06-18 16:45:14 -0500

    Skaffold backend app with express/prisma

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import { router } from "./routes";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(router);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
